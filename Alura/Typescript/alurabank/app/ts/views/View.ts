1b7c2f6d4ad51ba50ae4c231e80127c7a1abbee1
Rodrigo Marnon Veloso Barros de Morais <rodrigo.demorais@ambevtech.com.br>
2021-06-20 22:10:56 -0300

    usando exemplo de namespace

--- Alura/Typescript/alurabank/app/ts/views/View.ts
+++ Alura/Typescript/alurabank/app/ts/views/View.ts
@@ -1,14 +1,16 @@
-abstract class View<T> {
+namespace  Views {
+    export abstract class View<T> {
 
-    protected _elemento: JQuery;
-
-    constructor(seletor: string) {
-        this._elemento = $(seletor);
+        protected _elemento: JQuery;
+    
+        constructor(seletor: string) {
+            this._elemento = $(seletor);
+        }
+    
+        update(model: T) {
+            this._elemento.html(this.template(model));
+        }
+    
+        abstract template(model: T): string;    
     }
-
-    update(model: T) {
-        this._elemento.html(this.template(model));
-    }
-
-    abstract template(model: T): string;    
 }
