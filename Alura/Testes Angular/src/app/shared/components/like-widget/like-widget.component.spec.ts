3b9d8803e226ae01b8a66e32e5b9b22489433b50
Rodrigo Marnon Veloso Barros de Morais <rodrigo.demorais@ambevtech.com.br>
2021-09-27 06:01:29 -0300

    Trabalhando com asserções assincronas

--- Alura/Testes Angular/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ Alura/Testes Angular/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,43 +1,47 @@
-import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LikeWidgetComponent } from './like-widget.component';
 import { LikeWidgetModule } from './like-widget.module';
 
 describe(LikeWidgetComponent.name, () => {
 
   let fixture: ComponentFixture<LikeWidgetComponent>;
+  let component: LikeWidgetComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LikeWidgetModule],
-      providers: [{
-        provide: ComponentFixtureAutoDetect,
-        useValue: true
-      }]
-    })
-      .compileComponents();
+      imports: [LikeWidgetModule]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LikeWidgetComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create', () => {
-    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 
   it('Shoyld auto generate ID when is input property is missing', () => {
-    const component = fixture.componentInstance;
-    //fixture.detectChanges();
+    fixture.detectChanges();
 
     expect(component.id).toBeTruthy()
   });
 
   it('Shoult NOT generate ID when id input is present', () =>{
-    const component = fixture.componentInstance;
     const id = 'someId';
     component.id = id;
 
-    //fixture.detectChanges();
+    fixture.detectChanges();
     expect(component.id).toBe(id);
   });
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+    Should trigger emission when called`, (done) => {
+      fixture.detectChanges();
+      component.liked.subscribe(() => {
+        expect(true).toBeTrue();
+        done();
+      });
+      component.like();
+    });
 
 });
