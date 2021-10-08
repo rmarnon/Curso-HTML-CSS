c0faa66099fccfb309e9b8191a48222be03889d3
Rodrigo Marnon Veloso Barros de Morais <rodrigo.demorais@ambevtech.com.br>
2021-10-08 18:35:18 -0300

    Criando launchers personalizados

--- Alura/Testes Angular/karma.conf.js
+++ Alura/Testes Angular/karma.conf.js
@@ -31,6 +31,12 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      CustomFirefox: {
+        base: 'Firefox',
+        flags: ['-headless']
+      }
+    }
   });
 };
