189dce2dc58856c444dfc9bd50205d52c008c8e4
Rodrigo Marnon Veloso Barros de Morais <rodrigo.demorais@ambevtech.com.br>
2021-12-15 08:11:41 -0300

    Craindo componente photo list e ajustando load e app component

--- Alura/Testes Angular/src/app/shared/components/photo-board/services/photo-board.service.ts
+++ Alura/Testes Angular/src/app/shared/components/photo-board/services/photo-board.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { Photo } from '../interfaces/photo';
 
 @Injectable()
@@ -9,7 +10,9 @@ export class PhotoBoardService {
   constructor(private readonly httpClient: HttpClient) {}
 
   public getPhotos(): Observable<Photo[]> {
-    return this.httpClient.get<Photo[]>('http://localhost:3000/photos');
+    return this.httpClient
+    .get<Photo[]>('http://localhost:3000/photos')
+    .pipe(delay(1000));
   }
 
 }
